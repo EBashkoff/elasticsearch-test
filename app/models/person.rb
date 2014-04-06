ad538cc3e8fe0814be47645fd6b85cfc10a1af3a
EBashkoff <eric@fivestreet.com>
2014-04-06 14:20:20 -0400

    Add elasticsearch reindex rakefile

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,8 +2,6 @@ require 'elasticsearch/model'
 
 class Person < ActiveRecord::Base
 	attr_accessible :age, :email, :f_name, :l_name
-	# include Tire::Model::Search
-  # include Tire::Model::Callbacks
 
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
@@ -17,35 +15,11 @@ class Person < ActiveRecord::Base
     indexes :age, type: 'integer' # , index: 'not analyzed'
   end
 
-  # def as_indexed_json(options={})
-  #   as_json(only: [:id, :upc, :title, :description, :manufacturer_id]).merge({
-  #     'tag'          => tags,
-  #     'manufacturer' => manufacturer.name,
-  #     'in-stock'     => product_status.in_stock,
-  #     'price'        => product_status.price_cents
-  #   })
-  # end
-
   private
 
   def update_index
     __elasticsearch__.update_document
   end
-
-  # def self.search(params)
-  #   queryexp = {}
-  #   queryexp[:query] =  {
-  #     { term:  { f_name:  params[:search] } },
-  #     { term:  { l_name:  params[:search] } },
-  #     { term:  { email:   params[:search] } },
-  #     { term:  { age:     params[:search] } }
-  #   }
-  # end 
-
-  # def self.search(params)
-  # 	tire.search(load: true) do
-  #   	query { string params[:search]} if params[:search].present?
-  #   end
-  # end
 end
+
 
